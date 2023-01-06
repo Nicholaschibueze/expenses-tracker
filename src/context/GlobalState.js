882ab4be0462784a37fc4fe9bf7b0c510626ed2e
Nicholaschibueze <nicholaschibuezemichael@gmail.com>
2023-01-06 05:47:06 -1100

    first commit

---
+++ src/context/GlobalState.js
@@ -0,0 +1,54 @@
+import React, { createContext, useReducer} from 'react';
+
+import AppReducer from './AppReducer';
+
+ 
+
+// Initial state
+
+const initialState = {
+    transactions:  []
+
+
+}
+
+// Create Context 
+
+export const GlobalContext = createContext(initialState);
+
+
+// Provider  Component 
+
+export const GlobalProvider = ({ children}) =>{
+    const  [state, dispatch] = useReducer(AppReducer, initialState);
+
+    // Actions 
+    function deleteTransaction(id){
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            playload: id 
+
+        })
+    }
+
+
+    function addTransaction(transaction){
+        dispatch({
+            type: 'ADD_TRANSACTION',
+            playload: transaction
+
+        })
+    }
+
+
+    return(<GlobalContext.Provider value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction
+
+    }}>
+          {children}
+    </GlobalContext.Provider>);
+
+}
+
